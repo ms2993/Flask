8529f2168392cd72d1a437bbe44e4e2c58736198
Mallika <Mallikashree29@gmail.com>
2021-04-25 04:09:32 +0530

    version 2

---
+++ Market/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask,redirect,url_for,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+application=Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketdb.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(application)
--- run.py
+++ run.py
@@ -1,12 +1,2 @@
-from flask import Flask,redirect,url_for,render_template,request
-from flask_sqlalchemy import SQLAlchemy
-
-application=Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketdb.db'
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(application)
-
-
-if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
-    application.run(port=5000,debug=True)
+from Market import application
+application.run(port=5000,debug=True)
