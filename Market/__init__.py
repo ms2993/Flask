e10984fe9ff60ee4a01cbfa38963fa2c9e1d1fb8
Mallika <Mallikashree29@gmail.com>
2021-07-28 12:36:02 +0530

    version 5

--- Market/__init__.py
+++ Market/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask,redirect,url_for,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 
 application=Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketdb.db'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketdata.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SECRET_KEY'] = 'C\xa2\x16rP\x8b\xdd\xbd\x80K\xb7\xd4'
 db = SQLAlchemy(application)
 
 from Market import route
--- Market/route.py
+++ Market/route.py
@@ -1,7 +1,8 @@
 from Market import application
-from flask import render_template
-from Market.models import Item
-
+from flask import render_template,redirect, url_for,flash
+from Market.models import Item,User
+from Market.forms import Registrationform
+from Market import db
 @application.route('/')
 @application.route('/home')
 def home_page():
@@ -11,3 +12,30 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@application.route('/register', methods = ["GET", "POST"])
+def register_page():
+    form = Registrationform()
+    print(form.password1.data)
+    print(form.password2.data)
+    print(form.errors)
+    if form.is_submitted():
+        print("submitted")
+
+    if form.validate():
+        print("valid")
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        print('--test 0 --')
+        user_to_create = User(username=form.username.data,
+                              email=form.email.data,
+                              password=form.password1.data)
+        print('--test1--')
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        print('hello')
+        for err in form.errors.values():
+            flash(f'error in creating user: {err}', category='danger')
+    return render_template('register.html',form =form)
